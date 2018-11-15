36c8a574600f88c51333123a6b6636963d7b4d2f
Fedor Paretsky <me@f3d0r.com>
2018-11-14 22:01:25 -0800

    Finalize PDF export script with puppeteer

--- app.js
+++ app.js
@@ -1,83 +1,67 @@
-var request = require('request');
-var pdf = require('html-pdf');
-var fs = require('fs');
+//LOCAL IMPORTS
+var puppeteer = require('puppeteer');
 var cheerio = require('cheerio');
-var pLimit = require('p-limit');
 var pdfmerger = require('pdfmerger');
+var path = require('path');
+var fs = require('fs');
 var del = require('del');
+var pdfjsLib = require('pdfjs-dist');
 
-const pdfOptions = {
-    format: 'Letter'
-};
-const limit = pLimit(20);
-const baseURL = 'http://paulgraham.com/'
+//CONSTANTS
+const baseURL = 'http://paulgraham.com/';
+const essaySelector = 'body > table > tbody > tr > td:nth-child(3) > table:nth-child(5) > tbody';
+const finalExportPath = 'All_PG_Essays.pdf';
+const exportPath = path.join(__dirname, finalExportPath);
+
+//MAIN SCRIPT
 execute();
 
 async function execute() {
-    var mainPage = await getURL(baseURL + 'articles.html');
-    const $ = cheerio.load(mainPage);
-    var childNum = $('body > table > tbody > tr > td:nth-child(3) > table:nth-child(5) > tbody').children().length;
-    var reqs = [];
-    var essayNames = []
-    for (var i = 2; i <= childNum; i += 2) {
-        var essayURL = $('body > table > tbody > tr > td:nth-child(3) > table:nth-child(5) > tbody > tr:nth-child(' + i + ') > td > font > a').attr('href');
-        var essayName = $('body > table > tbody > tr > td:nth-child(3) > table:nth-child(5) > tbody > tr:nth-child(' + i + ') > td > font > a').text();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(baseURL + 'articles.html', {
+        waitUntil: 'networkidle2'
+    });
+    var bodyHTML = await page.evaluate(() => document.body.innerHTML);
+    await browser.close();
+    const $ = cheerio.load(bodyHTML);
+    var childNum = $(essaySelector).children().length;
+    console.log("EXPORTING " + (childNum / 2) + " PAUL GRAHAM ESSAYS. :)");
+    var pdfs = [];
+    for (var i = 2; i <= childNum / 16; i += 2) {
+        var essayURL = $(essaySelector + ' > tr:nth-child(' + i + ') > td > font > a').attr('href');
         if (essayURL.length >= 5 && essayURL.substring(essayURL.length - 5, essayURL.length) == ".html") {
-            reqs.push(limit(() => getURL(baseURL + essayURL)));
-            essayNames.push(essayName
-                .toLowerCase()
-                .split(' ').join('_')
-                .split('/').join('_'));
+            var currentPdfPath = await exportPDF(baseURL + essayURL, i);
+            pdfs.push(currentPdfPath);
+            console.log("DONE WITH ESSAY " + (i / 2) + " OUT OF " + (childNum / 32) + ".");
         }
     }
-    var essays = await Promise.all(reqs);
-    console.log("ESSAYS: " + essays.length);
 
-    var essayConversionReqs = [];
-    for (var i = 0; i < essays.length; i++) {
-        essayConversionReqs.push(limit(() => new Promise(function (resolve, reject) {
-            pdf.create(essays[i], pdfOptions).toFile('./exports/' + essayNames[i] + '.pdf', function (err, res) {
-                if (err) {
-                    console.log("HERE!");
-                    reject(err);
-                } else {
-                    resolve(res);
-                }
-            });
-        })));
-    }
-    resultPaths = [];
-    for (var i = 0; i < essayConversionReqs.length; i++) {
-        try {
-            var path = await essayConversionReqs[i];
-            resultPaths.push(path);
-        } catch (e) {
-            console.log("HERE!")
-        }
-    }
-    var pdfStream = pdfmerger(resultPaths);
-    var writeStream = fs.createWriteStream('all_essays.pdf');
+    var pdfStream = pdfmerger(pdfs);
+    var writeStream = fs.createWriteStream(exportPath);
     pdfStream.pipe(writeStream);
-    pdfmerger(resultPaths, 'all_essays.pdf');
+    pdfmerger(pdfs, exportPath);
     pdfStream.on('close', function (code) {
         del(['exports/*.pdf']).then(paths => {
-            console.log('Completed!');
+            pdfjsLib.getDocument(exportPath).then(function (doc) {
+                var numPages = doc.numPages;
+                console.log("COMPLETED! EXPORTED " + pdfs.length + " PAUL GRAHAM ESSAYS WITH " + numPages + " TOTAL PAGES.");
+            });
         });
     });
 }
 
-function getURL(url) {
-    var options = {
-        method: 'GET',
-        url: url,
-    };
-    return new Promise(function (resolve, reject) {
-        request(options, function (error, response, body) {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(body);
-            }
-        });
+async function exportPDF(url, index) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url, {
+        waitUntil: 'networkidle2'
     });
+    var currentPath = path.join(__dirname, 'exports/' + index + '.pdf');
+    await page.pdf({
+        path: currentPath,
+        format: 'A4'
+    });
+    await browser.close();
+    return currentPath;
 }
